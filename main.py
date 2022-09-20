96d88093e958d436f71fd0dba33e30a8f3f4dce5
Boris <borissicsac@gmail.com>
2022-09-20 09:53:36 -0300

    more ch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from dash import Dash
 from dash_bootstrap_components.themes import BOOTSTRAP
 from src.components.layout import create_layout
-from app import app, server
 
 def main() -> None:    
     app = Dash(external_stylesheets=[BOOTSTRAP])
