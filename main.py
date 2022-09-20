db388c0f2bd93c54bc267432f75d892bddd0ca3b
Boris <borissicsac@gmail.com>
2022-09-20 18:34:23 -0300

    Changed dataset, added filters and pie chart

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import flask
 from dash import Dash
 from dash_bootstrap_components.themes import BOOTSTRAP
 from src.components.layout import create_layout
+from src.data.loader import load_transaction_data
 
+DATA_PATH = "./data/transactions.csv"
+
+data = load_transaction_data(DATA_PATH)
 app = Dash(external_stylesheets=[BOOTSTRAP])
-app.title = "Medal dashboard"
-app.layout = create_layout(app)
+app.title = "Boris Dashboard"
+app.layout = create_layout(app, data)
 server = app.server
+
 def main() -> None:    
     # app = Dash(external_stylesheets=[BOOTSTRAP])
     # server=app.server
