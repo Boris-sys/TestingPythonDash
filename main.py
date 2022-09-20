96c6a8113361254c85cef3f6f35b84db04e39671
Boris <borissicsac@gmail.com>
2022-09-20 09:42:49 -0300

    changes main

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from dash import Dash
 from dash_bootstrap_components.themes import BOOTSTRAP
-
 from src.components.layout import create_layout
 
 app = Dash(external_stylesheets=[BOOTSTRAP])
 server=app.server
 
 
-def main() -> None:    
-    app.title = "Medal dashboard"
-    app.layout = create_layout(app)
-    app.run()
+app.title = "Medal dashboard"
+app.layout = create_layout(app)
+app.run()
 
 
 if __name__ == "__main__":
