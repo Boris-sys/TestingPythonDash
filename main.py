33737e120e28e670e35ca8fbcb97f56ef23945a3
Boris <borissicsac@gmail.com>
2022-09-20 17:29:11 -0300

    main order

--- main.py
+++ main.py
@@ -4,16 +4,13 @@ from dash_bootstrap_components.themes import BOOTSTRAP
 from src.components.layout import create_layout
 
 app = Dash(external_stylesheets=[BOOTSTRAP])
+app.title = "Medal dashboard"
 app.layout = create_layout(app)
 server = app.server
 def main() -> None:    
-    
-    
     # app = Dash(external_stylesheets=[BOOTSTRAP])
     # server=app.server
-
-    app.title = "Medal dashboard"
-    
+    #app.title = "Medal dashboard"
     app.run()
 
 
